fd153ecee889e91051048e63d3e57511b4bb60d3
Fellipe Rego <felliperego@hotmail.com>
2020-08-05 21:09:04 -0300

    setimo modulo - java-12

---
+++ java-12/src/main/java/com/challenge/service/impl/UserService.java
@@ -0,0 +1,43 @@
+package com.challenge.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.challenge.entity.User;
+import com.challenge.repository.UserRepository;
+import com.challenge.service.interfaces.UserServiceInterface;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserServiceInterface , UserDetailsService{
+
+    private UserRepository userRepository;
+
+    @Override
+    public Optional<User> findById(Long userId) {
+        return userRepository.findById(userId);
+    }
+
+    @Override
+    public List<User> findByAccelerationName(String name) {
+        return userRepository.findByCandidatesIdAccelerationName(name);
+    }
+
+    @Override
+    public List<User> findByCompanyId(Long companyId) {
+        return userRepository.findByCandidatesIdCompanyId(companyId);
+    }
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return this.userRepository.findByEmail(username).get();
+	}
+
+}
